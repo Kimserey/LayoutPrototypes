69766197d2ef7d4da0bf361be72ff911f718f1ef
Kimserey Lam <kimserey.lam@gmail.com>
2016-08-15 00:12:25 +0100

    layout

--- LayoutPrototypes/LayoutPrototypes.cs
+++ LayoutPrototypes/LayoutPrototypes.cs
@@ -1,29 +1,50 @@
 ﻿using System;
-
+using System.Collections;
+using System.Collections.Generic;
 using Xamarin.Forms;
 
 namespace LayoutPrototypes
 {
+
+	public class ItemPage : ContentPage {
+		public ItemPage()
+		{
+			this.Title = "Items";
+				
+			var layout = new AbsoluteLayout();
+			var currentTotal = new StackLayout {
+				Padding = new Thickness { Top = 20, Bottom = 20 }
+			};
+			var label = new Label { Text = "Current total", Style = Device.Styles.TitleStyle, HorizontalTextAlignment = TextAlignment.Center };
+			var total = new Label { Text = "$35.10", HorizontalTextAlignment = TextAlignment.Center };
+			currentTotal.Children.Add(label);
+			currentTotal.Children.Add(total);
+			layout.Children.Add(currentTotal, new Rectangle(0, 0, 1, .4), AbsoluteLayoutFlags.All);
+
+			var listLayout = 
+				new ListView {
+					ItemsSource = new List<String> { "Hello", "World", "Hi", "Bonjour", "Holla", "Salut", "Yo", "Monday", "Thursday", "Hello", "World", "Hi", "Bonjour", "Holla", "Salut", "Yo", "Monday", "Thursday" },
+					ItemTemplate =
+						new DataTemplate(() => {
+							var template = new TextCell();
+							template.SetBinding(TextCell.TextProperty, ".");
+							return template;
+						})
+				};
+			var scroll = new ScrollView { Content = listLayout };
+			scroll.BackgroundColor = Color.Aqua;
+
+			layout.Children.Add(scroll, new Rectangle(0, .4, 1, .4), AbsoluteLayoutFlags.All);
+			this.Content = layout;
+		}
+
+	}
+
 	public class App : Application
 	{
 		public App()
 		{
-			// The root page of your application
-			var content = new ContentPage
-			{
-				Title = "LayoutPrototypes",
-				Content = new StackLayout
-				{
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							HorizontalTextAlignment = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
-					}
-				}
-			};
-
+			var content = new ItemPage();
 			MainPage = new NavigationPage(content);
 		}
 
